c8cda315596ea7ad179e453224d12f01d465f3f8
x97574323 <canrollas@std.iyte.edu.tr>
2020-10-19 09:28:40 +0000

    example sep

--- lab1/example1.py
+++ lab1/example1.py
@@ -4,9 +4,3 @@ rousters=all_animals-chicken
 print(rousters)
 
 
-x=1
-y=4 
-z=0.25
-
-final_result=(((2*x+y)**2)*(z**(1/2)))/(x**(1/2)+y**(1/2))
-print(final_result)
---
+++ lab1/example2.py
@@ -0,0 +1,6 @@
+x=1
+y=4 
+z=0.25
+
+final_result=(((2*x+y)**2)*(z**(1/2)))/(x**(1/2)+y**(1/2))
+print(final_result)
