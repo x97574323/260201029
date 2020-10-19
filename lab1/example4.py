619a286839ebec9cbdf9484c1108e8a837091baa
x97574323 <canrollas@std.iyte.edu.tr>
2020-10-19 09:43:26 +0000

    new

---
+++ lab1/example4.py
@@ -0,0 +1,3 @@
+first=float(input("Pls input a celcius:"))
+T=first*1.8+32
+print(T)
---
+++ lab1/example5.py
@@ -0,0 +1,4 @@
+first=float(input("x:"))
+second=float(input("y:"))
+hipotenus=(first**2+second**2)**(1/2)
+print(hipotenus)
