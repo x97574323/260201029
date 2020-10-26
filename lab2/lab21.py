5990d1a692782e3d3964aff3a6fce2c7f1c1adaf
x97574323 <canrollas@std.iyte.edu.tr>
2020-10-26 08:32:17 +0000

    New

---
+++ lab2/lab21.py
@@ -0,0 +1,10 @@
+minumum_sayı=1
+maximum_sayı=100
+
+girdi=int(input("enter your number"))
+
+
+if(1<girdi<100):
+  print("Sayı 1 ile 100 arasında yer almaktadır")
+else:
+  print("Sayı verilen aralıkta yer almamaktadır.")
---
+++ lab2/lab22.py
@@ -0,0 +1,7 @@
+number=float(input("Pls input your number:"))
+
+
+if number<0:
+  print("Absolute value of the number is {}".format(number*(-1)))
+else:
+  print("Absolute value of the number is {}".format(number))
