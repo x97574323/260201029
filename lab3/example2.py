7d33c648bf1bae521247ca9f8eb3287245c291c1
x97574323 <canrollas@std.iyte.edu.tr>
2020-11-09 09:01:24 +0000

    ne enter

---
+++ lab3/example2.py
@@ -0,0 +1,12 @@
+year=int(input("Pls input the year:"))
+
+if(year%400==0):
+  print("Leap year")
+else:
+  if(year%4==0):
+    if(year%100==0):
+      print("İt is not leap year")
+    else:
+      print("İt is leap year")
+  else:
+    print("İt is not leap year")
