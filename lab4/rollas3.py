ec30caff9effb7ac298988d7ad3b7307bbd2ac5f
x97574323 <canrollas@std.iyte.edu.tr>
2020-11-16 09:28:08 +0000

    new

---
+++ lab4/rollas3.py
@@ -0,0 +1,11 @@
+number1="1352"
+number2="901052"
+counter=0
+counter1=0
+for i in number1:
+  
+  if i in number2:
+    number2=number2[counter1:]
+    counter=counter+1
+  counter1=counter1+1
+print(counter)
