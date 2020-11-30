fd04132203c60e395e0cc313abc813cb7b67b3a2
x97574323 <canrollas@std.iyte.edu.tr>
2020-11-30 09:01:42 +0000

    second

--- lab6/example2.py
+++ lab6/example2.py
@@ -2,6 +2,6 @@ books = ["ULYSSES","ANIMAL FARM","BRAVE NEW WORLD","ENDER'S GAME"]
 empty=dict()
 for i in books:
   a=len(i)
-  b=(len(set(list(i))))
+  b=(len(set((i))))
   empty[i]=(a,b)
 print(empty)
