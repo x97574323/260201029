9abc7d87e15f64c51f9154e9347df9723f9aa8cd
x97574323 <canrollas@std.iyte.edu.tr>
2020-12-14 08:51:47 +0000

    Newest

--- lab7/soru1.py
+++ lab7/soru1.py
@@ -2,6 +2,8 @@ def summer(x):
   local=0
   for i in x:
     local=local+i
-  return local
-print("Sonuc:")
-print(summer([12,-7,5,-89.4,3,27,56,57.3]))
+  return local**2
+def main():
+  print("Sonuc:")
+  print(summer([12,-7,5,-89.4,3,27,56,57.3]))
+main()
