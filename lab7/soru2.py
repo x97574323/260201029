2509fb8d7e74e71bde7f95f65fe8da4419d3e488
x97574323 <canrollas@std.iyte.edu.tr>
2020-12-14 09:08:37 +0000

    new one with one function

--- lab7/soru2.py
+++ lab7/soru2.py
@@ -2,10 +2,11 @@ def rollas(x,y):
   ranger=range(x,y)
   a=list()
   for i in ranger:
-    if(i%2!=0):
-      a.append(i)
-  return(a)
-
+    for m in reversed(range(2,i-1)):
+      if(i%m==0):
+        a.append(i)
+        break       
+  return(sorted(list(set(ranger)-set(a))))
 def main():
   x=int(input("Pls enter:"))
   y=int(input("Pls enter:"))
