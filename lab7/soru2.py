97f4dbaca74567b5f162db47fe46b6f3892f868d
x97574323 <canrollas@std.iyte.edu.tr>
2020-12-14 09:14:15 +0000

    newst

--- lab7/soru2.py
+++ lab7/soru2.py
@@ -1,14 +1,16 @@
 def rollas(x,y):
   ranger=range(x,y)
   a=list()
+  setter={1,0}
   for i in ranger:
     for m in reversed(range(2,i-1)):
       if(i%m==0):
         a.append(i)
         break       
-  return(sorted(list(set(ranger)-set(a))))
+  return(sorted(list(set(ranger)-set(a)-setter)))
 def main():
   x=int(input("Pls enter:"))
   y=int(input("Pls enter:"))
   print(rollas(x,y))
+
 main()
