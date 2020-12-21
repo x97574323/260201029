37c4cac7c98c67317aa58c7cc891673300033927
x97574323 <canrollas@std.iyte.edu.tr>
2020-12-21 09:13:24 +0000

    newer

--- lab9/example3.py
+++ lab9/example3.py
@@ -1,12 +1,9 @@
 def evens(n):
-  if(len(n)==1):
-    if(n[0]%2==0):
-      return 1
-    else:
-      return 0
+  if(len(n)==0):
+    return 0
   else:
     if(n[-1]%2==0):
       return 1+evens(n[:-1])
     else:
       return evens(n[:-1])
-print(evens([1,2,3,4,5]))
+print(evens([1,2,3,4,5,6,9]))
